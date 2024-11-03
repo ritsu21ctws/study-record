51d7b7daef2281f06e5be25691c7ba37d4b769d4
ritsu <ritsu.21.ctws@gmail.com>
2024-11-03 20:19:56 +0900

    supabaseへの登録処理の実装

--- utils/supabaseFunctions.jsx
+++ utils/supabaseFunctions.jsx
@@ -4,3 +4,8 @@ export const getAllRecords = async () => {
   const records = await supabase.from('study-record').select();
   return records.data;
 }
+
+export const addRecord = async (record) => {
+  const result = await supabase.from('study-record').insert(record);
+  return result;
+}

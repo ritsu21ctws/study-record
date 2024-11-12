459843d68e44936762eac95f532c7db93c03d460
ritsu <ritsu.21.ctws@gmail.com>
2024-11-13 00:17:26 +0900

    テストの修正

--- src/tests/componentApp.spec.jsx
+++ src/tests/componentApp.spec.jsx
@@ -6,12 +6,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 describe("Title Test", () => {
   it("タイトルが「学習記録一覧」であること", async () => {
     render(<App />);
-    // loading画面の表示が終わるまで待機
-    await waitFor(() => {
-      expect(screen.getByTestId("title")).toBeInTheDocument();
-    });
-    // タイトルの取得
-    const title = screen.getByTestId("title");
+    const title = await screen.findByTestId("title");
     expect(title).toHaveTextContent("学習記録一覧");
   });
 });

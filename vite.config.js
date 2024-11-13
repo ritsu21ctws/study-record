290e1e030b863684175b6f8ab17d39f8e48ab3fc
ritsu <ritsu.21.ctws@gmail.com>
2024-11-13 23:45:40 +0900

    npm run devの「ReferenceError: env is not defined」エラーの解消

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import env from "vite-plugin-env-compatible"
 
 // https://vite.dev/config/
 export default defineConfig({
